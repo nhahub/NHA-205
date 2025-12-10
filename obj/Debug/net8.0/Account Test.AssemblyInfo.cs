07fce43cb57d4b8dc22f6dd307baf080c9db143a
Sam <illusorist@users.noreply.github.com>
2025-12-10 08:20:36 -0500

    Fixed updated login pages

--- obj/Debug/net8.0/Account Test.AssemblyInfo.cs
+++ obj/Debug/net8.0/Account Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Account Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f364edb17b040000f966fc8536333a1f1a7e71e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a31afe92a89c88fde91b53715929171ae3c2e84b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Account Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Account Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
